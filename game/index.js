0c927e6cf9819f281caba46402bf0fb3be25d0b3
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-05-20 20:10:06 -0800

    feat: Create player and enemy characters including gravity field fallout.

--- game/index.js
+++ game/index.js
@@ -2,7 +2,59 @@ const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
 //window resolution=>16:9
-canvas.width=1024;
-canvas.height=576;
+canvas.width = 1024;
+canvas.height = 576;
 
-c.fillRect(0,0,canvas.width,canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 0.2;
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+
+    const isWithInCanvas =
+      this.position.y + this.height + this.velocity.y >= canvas.height;
+      
+    if (isWithInCanvas) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+    }
+  }
+}
+
+const player = new Sprite({
+  position: { x: 0, y: 0 },
+  velocity: { x: 0, y: 2 },
+});
+// player.draw();
+
+const enemy = new Sprite({
+  position: { x: 500, y: 0 },
+  velocity: { x: 0, y: 2 },
+});
+// enemy.draw();
+console.log(player);
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+  // console.log("HI")
+}
+
+animate();
