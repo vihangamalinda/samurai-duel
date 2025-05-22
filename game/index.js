d56b7238dbe33934c639f179fca61f75b2782f4f
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-05-21 18:44:41 -0800

    feat: Adding player and enemy movements.

--- game/index.js
+++ game/index.js
@@ -7,27 +7,37 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity = 0.2;
+const gravity = 0.7;
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
     this.height = 150;
+    this.width = 50;
+    this.lastKey;
   }
 
   draw() {
     c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, 50, this.height);
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
   update() {
     this.draw();
+    const isWithinCanvaseWalls =
+      this.position.x + this.velocity.x >= 0 &&
+      this.position.x + this.velocity.x + this.width <= canvas.width;
+
+    //position movement update
+    if (isWithinCanvaseWalls) {
+      this.position.x += this.velocity.x;
+    }
     this.position.y += this.velocity.y;
 
-    const isWithinCanvas =
+    const isWithinCanvasBottom =
       this.position.y + this.height + this.velocity.y >= canvas.height;
 
-    if (isWithinCanvas) {
+    if (isWithinCanvasBottom) {
       this.velocity.y = 0;
     } else {
       this.velocity.y += gravity;
@@ -48,13 +58,96 @@ const enemy = new Sprite({
 // enemy.draw();
 console.log(player);
 
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  arrowRight: {
+    pressed: false,
+  },
+  arrowLeft: {
+    pressed: false,
+  },
+};
+
+let lastKey;
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
+
+  //reset player's And enemey's x-axis velocity
+  player.velocity.x = 0;
+  enemy.velocity.x = 0;
+
+  // Player's velocity update to perform movements
+  if (keys.a.pressed && player.lastKey === "a") {
+    player.velocity.x = -5;
+  } else if (keys.d.pressed && player.lastKey === "d") {
+    player.velocity.x = 5;
+  }
+
+  // Enemy's velocity update to perform movements
+  if (keys.arrowRight.pressed && enemy.lastKey === "ArrowRight") {
+    enemy.velocity.x = 5;
+  } else if (keys.arrowLeft.pressed && enemy.lastKey === "ArrowLeft") {
+    enemy.velocity.x = -5;
+  }
   // console.log("HI")
 }
-
 animate();
+
+window.addEventListener("keydown", (event) => {
+  console.log(event.key);
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      player.lastKey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      player.lastKey = "a";
+      break;
+    case "w":
+      if (player.velocity.y === 0) {
+        player.velocity.y = -20;
+      }
+      break;
+    case "ArrowRight":
+      keys.arrowRight.pressed = true;
+      enemy.lastKey = "ArrowRight";
+      break;
+    case "ArrowLeft":
+      keys.arrowLeft.pressed = true;
+      enemy.lastKey = "ArrowLeft";
+      break;
+    case "ArrowUp":
+      if (enemy.velocity.y === 0) {
+        enemy.velocity.y = -20;
+      }
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "ArrowRight":
+      keys.arrowRight.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.arrowLeft.pressed = false;
+      break;
+  }
+});
