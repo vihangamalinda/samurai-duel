bd02f5b6fe90715eef007d41aefd361f2243587d
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-05-24 06:38:34 -0800

    refactor: Moving the Character class to a seperate file.

--- game/index.js
+++ game/index.js
@@ -1,3 +1,5 @@
+import Character from "./src/character.js";
+
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
@@ -19,69 +21,6 @@ canvas.width = 1280;
 canvas.height = 720;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
-
-const gravity = 0.7;
-class Character {
-  constructor({ position, velocity, color = "red", offset }) {
-    this.position = position;
-    this.velocity = velocity;
-    this.height = 150;
-    this.width = 50;
-    this.lastKey;
-    this.attackBox = {
-      position: { x: position.x, y: position.y },
-      width: 100,
-      height: 50,
-      offset,
-    };
-    this.color = color;
-    this.isAttacking;
-    this.health = 100;
-    console.log(this);
-  }
-
-  draw() {
-    c.fillStyle = this.color.body;
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-
-    //Attack box
-    if (this.isAttacking) {
-      const { position, width, height, offset } = this.attackBox;
-      c.fillStyle = this.color.attackBox;
-      c.fillRect(position.x + offset.x, position.y, width, height);
-    }
-  }
-
-  update() {
-    this.attackBox.position = { ...this.position };
-    this.draw();
-    const isWithinCanvaseWalls =
-      this.position.x + this.velocity.x >= 0 &&
-      this.position.x + this.velocity.x + this.width <= canvas.width;
-
-    //position movement update
-    if (isWithinCanvaseWalls) {
-      this.position.x += this.velocity.x;
-    }
-    this.position.y += this.velocity.y;
-
-    const isWithinCanvasBottom =
-      this.position.y + this.height + this.velocity.y >= canvas.height;
-
-    if (isWithinCanvasBottom) {
-      this.velocity.y = 0;
-    } else {
-      this.velocity.y += gravity;
-    }
-  }
-
-  attack() {
-    this.isAttacking = true;
-    setTimeout(() => {
-      this.isAttacking = false;
-    }, 100);
-  }
-}
 
 const player = new Character({
   position: { x: 0, y: 0 },
@@ -154,8 +93,11 @@ function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
-  player.update();
-  enemy.update();
+
+  const updateInfo = { canvasContext: c, canvas: canvas };
+
+  player.update(updateInfo);
+  enemy.update(updateInfo);
 
   //reset player's And enemey's x-axis velocity
   player.velocity.x = 0;
---
+++ game/src/character.js
@@ -0,0 +1,68 @@
+export default class Character {
+  constructor({ position, velocity, color = "red", offset }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+    this.width = 50;
+    this.lastKey;
+    this.attackBox = {
+      position: { x: position.x, y: position.y },
+      width: 100,
+      height: 50,
+      offset,
+    };
+    this.color = color;
+    this.isAttacking;
+    this.health = 100;
+    console.log(this);
+  }
+
+  draw(canvasContext) {
+    canvasContext.fillStyle = this.color.body;
+    canvasContext.fillRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+
+    //Attack box
+    if (this.isAttacking) {
+      const { position, width, height, offset } = this.attackBox;
+      canvasContext.fillStyle = this.color.attackBox;
+      canvasContext.fillRect(position.x + offset.x, position.y, width, height);
+    }
+  }
+
+  update({ canvasContext: canvasContext, canvas: canvas }) {
+    this.attackBox.position = { ...this.position };
+    this.draw(canvasContext);
+    const isWithinCanvaseWalls =
+      this.position.x + this.velocity.x >= 0 &&
+      this.position.x + this.velocity.x + this.width <= canvas.width;
+
+    //position movement update
+    if (isWithinCanvaseWalls) {
+      this.position.x += this.velocity.x;
+    }
+    this.position.y += this.velocity.y;
+
+    const isWithinCanvasBottom =
+      this.position.y + this.height + this.velocity.y >= canvas.height;
+
+    if (isWithinCanvasBottom) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += GRAVITY;
+    }
+  }
+
+  attack() {
+    this.isAttacking = true;
+    setTimeout(() => {
+      this.isAttacking = false;
+    }, 100);
+  }
+}
+
+const GRAVITY = 0.7;
